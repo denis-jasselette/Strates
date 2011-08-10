87e1b8f2d90157cefcd214dbf3204e429f1bfaf9
Denis Jasselette <denis.jasselette@yahoo.fr>
2011-08-10 14:53:25 +0200

    Let the mouse leave the window

--- src/cursor.cpp
+++ src/cursor.cpp
@@ -8,29 +8,20 @@ Cursor::Cursor(sf::RenderWindow *window, ImageManager *imageMgr) {
   const sf::Texture *image = imageMgr->get("cursor");
   sprite = new sf::Sprite(*image);
 
-  position = sf::Vector2i(window->GetWidth() / 2, window->GetHeight() / 2);
-  centerCursor();
+  refreshPosition();
 }
 
 Cursor::~Cursor() {
   delete sprite;
 }
 
-void Cursor::centerCursor() {
-  sf::Vector2i center(window->GetWidth() / 2, window->GetHeight() / 2);
-  sf::Mouse::SetPosition(center, *window);
-}
-
 void Cursor::refreshPosition() {
-  sf::Vector2i mouse = sf::Mouse::GetPosition(*window);
   int w = window->GetWidth();
   int h = window->GetHeight();
 
-  position += mouse - sf::Vector2i(w / 2, h / 2);
+  position = sf::Mouse::GetPosition(*window);
   position.x = clamp(position.x, 0, w - 1);
   position.y = clamp(position.y, 0, h - 1);
-
-  centerCursor();
 }
 
 sf::Vector2i Cursor::getPosition() {
