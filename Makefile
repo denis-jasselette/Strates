e0f69805e15bd6b374bfdcf70e71ad7fd50f1dce
Denis Jasselette <denis.jasselette@yahoo.fr>
2011-08-16 01:12:00 +0200

    Added doxygen config file and changed Makefile to build doc

--- Makefile
+++ Makefile
@@ -5,21 +5,28 @@ LDFLAGS := -lm -lsfml-graphics -lsfml-system
 
 EXEC := a.out
 EXPORT := export.tar.gz
+DOC := doc
 DIST = $(wildcard *)
 
-all: src_all res_all
-	cp -a $(src_EXEC) $(EXEC)
+all: $(EXEC) src_all res_all
 
 clean: src_clean res_clean
 	rm -rf $(EXEC)
 
 distclean: clean src_distclean res_distclean
+	rm -rf $(DOC)
 	rm -rf $(EXPORT)
 
 dist: distclean
 	tar czf $(EXPORT) $(DIST)
 
+$(DOC): Doxyfile
+	doxygen
+
 DIR := src
 include src/Rules.mk
 DIR := res
 include res/Rules.mk
+
+$(EXEC): $(src_EXEC)
+	cp -a $(src_EXEC) $(EXEC)
--- src/Rules.mk
+++ src/Rules.mk
@@ -5,20 +5,24 @@ SUFFIXES += .d
 SUBDIRS := $(DIR) $(DIR)/JSON
 src_EXEC := $(DIR)/a.out
 src_SRCS := $(foreach D,$(SUBDIRS),$(wildcard $D/*.cpp))
+src_HDRS := $(foreach D,$(SUBDIRS),$(wildcard $D/*.h))
 src_OBJS := $(src_SRCS:.cpp=.o)
 src_DEPS := $(src_SRCS:.cpp=.d)
 
 -include $(src_DEPS)
 
-src_all: $(src_EXEC) res_all
+src_all: $(src_EXEC)
 
 $(src_EXEC): $(src_OBJS)
 	$(CXX) $(CXXFLAGS) $(LDFLAGS) $(src_OBJS) -o $(src_EXEC)
 
 %.d: %.cpp
-	$(CXX) $(CXXFLAGS) -MM -MT '$(patsubst %.cpp,%.o,$<)' $< >$@
+	@echo Building dependency file for $<
+	@$(SHELL) -ec '$(CXX) -MM $< \
+	              | sed '\''s#\.o[ :]*#.o $@ : #g'\'' > $@; \
+	              [ -s $@ ] || rm -f $@'
 
-%.o: %.cpp %.d
+%.o: %.cpp
 	$(CXX) $(CXXFLAGS) -c $< -o $@
 
 src_clean:
@@ -26,3 +30,5 @@ src_clean:
 
 src_distclean: src_clean
 	rm -rf $(src_EXEC)
+
+$(DOC): $(src_HDRS) $(src_SRCS)
