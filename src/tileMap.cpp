b2be175892a0003c9452ca2093f720d956e21a0b
Denis Jasselette <denis.jasselette@gmail.com>
2013-09-28 22:49:58 +0200

    Removed the one pixel gap separating the tiles

--- src/tileMap.cpp
+++ src/tileMap.cpp
@@ -6,8 +6,8 @@ TileMap::TileMap(std::string name, ImageManager *imgMgr) {
   sprite = new sf::Sprite(*src);
   tileWidth = 32;
   tileHeight = 32;
-  rows = (src->getSize().y + 1) / (tileHeight + 1);
-  cols = (src->getSize().x + 1) / (tileWidth + 1);
+  rows = src->getSize().y / tileHeight;
+  cols = src->getSize().x / tileWidth;
 }
 
 TileMap::~TileMap() {
@@ -17,7 +17,7 @@ TileMap::~TileMap() {
 sf::Sprite *TileMap::get(unsigned int type) {
   int x = type % cols;
   int y = type / cols;
-  sf::IntRect rect(x * (tileWidth + 1), y * (tileHeight + 1), tileWidth, tileHeight);
+  sf::IntRect rect(x * tileWidth, y * tileHeight, tileWidth, tileHeight);
   sprite->setTextureRect(rect);
   return sprite;
 }
